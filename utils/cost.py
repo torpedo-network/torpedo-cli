35fd557bac5ff22e6eee1b7cd37a0d24c99dc716
Jay Ram <jaycram@gmail.com>
2023-02-17 23:20:19 -0500

    updated with place holders, added get_cost_from_torpedo_product

--- utils/cost.py
+++ utils/cost.py
@@ -4,12 +4,23 @@ from presets import presets
 
 # TODO: standardize with SKUs
 
-def get_cost(vendor_name = "aws", service = 'AmazonEC2', region = 'us-east-1', machine_type = 'm3.large'):
+def get_cost_from_torpedo_product(vendor_name="gcp",product='DemoSession'):
+    if vendor_name == 'gcp':
+        return get_cost(vendor_name=vendor_name,service='Compute Engine', region = 'us-west1', productFamily ='Compute Instance',machine_type=presets['gcp'][product])
+    elif vendor_name == 'aws':
+        # undefined, need to find good AWS instances first
+        return get_cost(machine_type=presets['aws'][product])
+def get_cost(vendor_name = "aws", service = 'AmazonEC2', region = 'us-east-1', productFamily=None, machine_type = 'm3.large'):
     headers = {
         'X-Api-Key': 'ico-ZhdTlKXsD4kUtB4ZuGOSl4L2adtD2u5f',
         'Content-Type': 'application/json',
     }
-    query_string = "{products(filter:{vendorName:\"%s\",service:\"%s\",region:\"%s\",attributeFilters:[{key:\"instanceType\",value:\"%s\"},{key:\"operatingSystem\",value:\"Linux\"},{key:\"tenancy\",value:\"Shared\"},{key:\"capacitystatus\",value:\"Used\"},{key:\"preInstalledSw\",value:\"NA\"}]}) {prices(filter:{purchaseOption:\"on_demand\"}) {USD}}}" % (vendor_name,service,region, machine_type)
+    if vendor_name=='gcp':
+        
+        query_string = "{products(filter:{vendorName:\"%s\",service:\"%s\", productFamily: \"%s\" region:\"%s\",attributeFilters:[{key:\"machineType\",value:\"%s\"}]}) {prices(filter:{purchaseOption:\"on_demand\"}) {USD}}}" % (vendor_name,service, productFamily, region, machine_type)
+    elif vendor_name=='aws': 
+        productFamily='EC2 THING'
+        query_string = "{products(filter:{vendorName:\"%s\",service:\"%s\", productFamily: \"%s\" region:\"%s\",attributeFilters:[{key:\"instanceType\",value:\"%s\"}]}) {prices(filter:{purchaseOption:\"on_demand\"}) {USD}}}" % (vendor_name,service, productFamily, region, machine_type)
     json_string = {"query": query_string}
 
 
@@ -17,5 +28,6 @@ def get_cost(vendor_name = "aws", service = 'AmazonEC2', region = 'us-east-1', m
 
     return (response.json())
 
-print(get_cost())
-print(get_cost(vendor_name="gcp",service='Compute Engine',machine_type=presets[0]['gcp'][0]['DataAnalyticsOffering']))
+
+#for key in presets['gcp'].keys():
+#    print(get_cost_from_torpedo_product(vendor_name="gcp",product=key))
--- utils/presets.py
+++ utils/presets.py
@@ -1,7 +1,7 @@
-presets = [{'gcp': [{'DataAnalyticsOffering': 'n1-standard-1'},
-    {'DemoSession' : 'n1-standard-64'},
-    {'MLExperimentsOffering' : 'n1-standard-64'},
-    {'DLExperimentsOffering' : 'n1-standard-64'},
-    {'DLPowerhouseOffering': 'n1-standard-64'}]},
-        {'aws': []},
-        {'azure': []}]
+presets = {'gcp': {'DataAnalyticsOffering': 'n1-standard-1',
+    'DemoSession' : 'n1-standard-1',
+    'MLExperimentsOffering' : 'n1-standard-16',
+    'DLExperimentsOffering' : 'n1-standard-32',
+    'DLPowerhouseOffering': 'n1-standard-64'},
+        'aws': {},
+        'azure': {}}
